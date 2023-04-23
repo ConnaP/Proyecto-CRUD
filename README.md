dd10740286da76a4aeacb61182f30a1a0a7fccc0
Constanza Palavecino <copalavecino.dwfsc6@bootcampudd.cl>
2023-04-22 21:53:19 -0400

    Add update, delete

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# proyecto-crud
+**ESTA ES LA DESCRICIÓN DEL PROYECTO**
+
+#### Contenido
+	1. Mantenedor de usuarios.
+
+#### Secciones:
+	1. Header
+	2. CRUD (create, read, update y delete)
+	3. Footer
+	
+
+#### Tecnologías utilizadas
+	1. HTML
+	2. CSS
+
+#### URL proyecto
+[Aquí](https://connap.github.io/proyecto-crud)
+
+#### Agradecimientos  
+> Bootcamp UDD
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,30 @@
-let users = [];
+let users = JSON.parse(localStorage.getItem("users")) || [];
 
 const create = (event) => {
-  event.preventDefault();
-  const user = readForm();
+  if (event) {
+    event.preventDefault();
+  }
+
+  const user = readAndSaveForm();
 
   if (user) {
     createRowTable(user);
+    saveUsersDataLocalStorage();
     clearForm();
-    saveDataLocalStorage(user);
   }
 };
 
 const addButton = document.getElementById("add");
 addButton.addEventListener("click", create);
 
-const readForm = () => {
+const readAndSaveForm = () => {
   const nameInput = document.getElementById("name");
   const lastnameInput = document.getElementById("lastname");
   const genderInput = document.getElementById("gender");
   const descInput = document.getElementById("description");
 
   const user = {
+    id: Date.now(),
     name: nameInput.value,
     lastname: lastnameInput.value,
     gender: genderInput.value,
@@ -48,36 +52,119 @@ const clearForm = () => {
   form.reset();
 };
 
-const createRow = (user) => {
+const createRowTable = (user) => {
   const tbody = document.getElementById("tbody");
 
   tbody.innerHTML += `
-          <tr>
+        <tr id="tr${user.id}">
+              <td>${user.id}</td>
               <td>${user.name}</td>
               <td>${user.lastname}</td>
               <td>${user.gender}</td>
               <td>${user.desc}</td>
              
               <td>
-                  <button class="edit btn btn-outline-warning btn-sm" onclick="editar()">Editar</button>
-                  <button class="delete btn btn-outline-danger btn-sm">Eliminar</button>
+                  <button class="edit btn btn-outline-warning btn-sm" onclick="editRowTable('${user.id}')">Editar</button>
+                  <button class="delete btn btn-outline-danger btn-sm" onclick="deleteRowTable('${user.id}')">Eliminar</button>
               </td>
-          </tr>
+        </tr>
       `;
 };
 
-const saveDataLocalStorage = (user) => {
-  users.push(user);
+const saveUsersDataLocalStorage = () => {
   localStorage.setItem("users", JSON.stringify(users));
 };
 
 const readFromLocalStorage = () => {
-  const users = JSON.parse(localStorage.getItem("users")) || [];
-  users.forEach(user => { 
-    createRow(user);
-    
+  users.forEach((user) => {
+    createRowTable(user);
   });
-  console.log(users);
+};
+
+const deleteRowTable = (idUser) => {
+  const positionUser = users.findIndex((user) => user.id == idUser);
+  users.splice(positionUser, 1);
+
+  saveUsersDataLocalStorage();
+
+  const idRowDelete = `tr${idUser}`;
+
+  const trTable = document.getElementById(idRowDelete);
+  trTable.remove();
+};
+
+const editRowTable = (idUser) => {
+  const idRowTable = `tr${idUser}`;
+
+  const tr = document.getElementById(idRowTable);
+
+  const childrens = Array.from(tr.children); // obtenemos los hijos del tr
+
+  const arrayValuesInputs = [];
+
+  childrens.forEach((children) => {
+    const value = children.innerHTML;
+
+    arrayValuesInputs.push(value);
+  });
+
+  const nameInput = document.getElementById("name");
+  nameInput.value = arrayValuesInputs[1];
+
+  const lastnameInput = document.getElementById("lastname");
+  lastnameInput.value = arrayValuesInputs[2];
+
+  const genderInput = document.getElementById("gender");
+  genderInput.value = arrayValuesInputs[3];
+
+  const descInput = document.getElementById("description");
+  descInput.value = arrayValuesInputs[4];
+
+  const btnAdd = document.getElementById("add");
+
+  if (btnAdd) {
+    btnAdd.remove();
+  }
+
+  const btnUpdate = document.createElement("button");
+  btnUpdate.id = "btn-uddate";
+  btnUpdate.name = idUser;
+  btnUpdate.classList.add("btn");
+  btnUpdate.classList.add("btn-outline-warning");
+  btnUpdate.classList.add("btn-sm");
+  btnUpdate.innerHTML = "Editar";
+  btnUpdate.addEventListener("click", update);
+
+  const contentBtn = document.getElementById("content-btn");
+
+  if (contentBtn.children.length == 0) {
+    contentBtn.appendChild(btnUpdate);
+  }
+
+  if (contentBtn.children.length > 0) {
+    btnUpdate.name = idUser;
+  }
+};
+
+update = () => {
+  const btnUpdate = document.getElementById("btn-uddate");
+
+  create();
+  deleteRowTable(btnUpdate.name);
+
+  const btnAdd = document.createElement("button");
+  btnAdd.id = "add";
+  btnAdd.classList.add("btn");
+  btnAdd.classList.add("btn-outline-success");
+  btnAdd.classList.add("btn-sm");
+  btnAdd.innerHTML = "Agregar";
+  btnAdd.addEventListener("click", create);
+
+  const contentBtn = document.getElementById("content-btn");
+
+  contentBtn.appendChild(btnAdd);
+
+  btnUpdate.remove();
 };
 
 readFromLocalStorage();
